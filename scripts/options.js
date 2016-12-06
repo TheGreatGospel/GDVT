c9e99f22e46e80b5ca315ba2327741c7ade683e4
TheGreatGospel <david.kaen.chan@gmail.com>
2016-12-06 18:01:42 +1300

    asdg

--- scripts/options.js
+++ scripts/options.js
@@ -1,10 +1,38 @@
+function conditionHack(cnd1, msg1, cnd2, msg2) {
+    if (cnd1) {
+        return msg1;
+    } else if (cnd2) {
+        return msg2;
+    } else {
+        return "";
+    }
+}
+
 function options_Validate() {
-    var flag = true;
+    var errorMsg = "";
     var inpObj = {
         popSize: Number($("#options_popSize").val()),
         numOfPop: Number($("#options_numOfPop").val()),
         init: Number($("#options_init").val())
     };
+
+    errorMsg += conditionHack(isNaN(inpObj.popSize),
+        "The \"Population Size\" has an invalid input! <br>",
+        inpObj.popSize < 50 || inpObj.popSize > 500,
+        "The \"Population Size\" has an invalid input! <br>");
+
+    if (isNaN(inpObj.numOfPop)) {
+        errorMsg += "The \"Number of Populations\" has an invalid input! <br>";
+        if (inpObj.popSize < 50 || inpObj.popSize > 500) {
+            errorMsg += "The \"Population Size\" has an invalid input! <br>";
+        } else {
+            settings.popSize = inpObj.popSize;
+        }
+    }
+
+    if (isNaN(inpObj.init)) {
+        errorMsg += "The \"Initial Number of Simulations\" has an invalid input! <br>";
+    }
 
     document.getElementById("demo").innerHTML =  typeof inpObj.popSize + " " +
         typeof inpObj.numOfPop + " " + typeof inpObj.init + "<br>" + 
