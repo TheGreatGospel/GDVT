8f88a8905abcf06b72a232b7a03cb5d6664bf607
TheGreatGospel <david.kaen.chan@gmail.com>
2016-12-06 14:30:47 +1300

    testing again

--- scripts/options.js
+++ scripts/options.js
@@ -1,9 +1,46 @@
 function options_Validate() {
+    var flag = true;
     var inpObj = {
         popSize: $("#options_popSize").val(),
         numOfPop: $("#options_numOfPop").val(),
         init: $("#options_init").val()
     };
 
-    document.getElementById("demo").innerHTML =  JSON.stringify(inpObj);
+    if (typeof inpObj.popSize === "number") {
+        if (inpObj.popSize < 50 || inpObj.popSize > 500) {
+            flag = false;
+        } else {
+            settings.popSize = inpObj.popSize;
+        }
+    } else {
+        flag = false;
+    }
+
+    if (typeof inpObj.numOfPop === "number") {
+        if (inpObj.numOfPop < 1 || inpObj.popSize > 10) {
+            flag = false;
+        } else {
+            settings.numOfPop = inpObj.numOfPop;
+        }
+    } else {
+        flag = false;
+    }
+
+
+    if (typeof inpObj.init === "number") {
+        if (inpObj.init < 1) {
+            flag = false;
+        } else {
+            settings.init = inpObj.init;
+        }
+    } else {
+        flag = false;
+    }
+
+    if (flag === true) {
+        document.getElementById("demo").innerHTML =  "proceed";
+    } else {
+        document.getElementById("demo").innerHTML =  JSON.stringify(inpObj);
+        alert("Alert!");
+    }
 }
