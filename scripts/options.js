f2b132a5c84e76504dc9483cf96adfb8d2c3a1fc
TheGreatGospel <david.kaen.chan@gmail.com>
2016-12-06 14:49:56 +1300

    testing

--- scripts/options.js
+++ scripts/options.js
@@ -6,41 +6,12 @@ function options_Validate() {
         init: $("#options_init").val()
     };
 
-    if (typeof inpObj.popSize === "number") {
-        if (inpObj.popSize < 50 || inpObj.popSize > 500) {
-            flag = false;
-        } else {
-            settings.popSize = inpObj.popSize;
-        }
-    } else {
-        flag = false;
-    }
-
-    if (typeof inpObj.numOfPop === "number") {
-        if (inpObj.numOfPop < 1 || inpObj.popSize > 10) {
-            flag = false;
-        } else {
-            settings.numOfPop = inpObj.numOfPop;
-        }
-    } else {
-        flag = false;
-    }
-
-
-    if (typeof inpObj.init === "number") {
-        if (inpObj.init < 1) {
-            flag = false;
-        } else {
-            settings.init = inpObj.init;
-        }
-    } else {
-        flag = false;
-    }
-
-    if (flag === true) {
+    document.getElementById("demo").innerHTML =  inpObj.popSize;
+    
+    /*if (flag === true) {
         document.getElementById("demo").innerHTML =  "proceed";
     } else {
-        document.getElementById("demo").innerHTML =  JSON.stringify(inpObj);
+       document.getElementById("demo").innerHTML =  JSON.stringify(inpObj);
         alert("Alert!");
-    }
+    }*/
 }
