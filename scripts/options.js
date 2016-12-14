c181f8577e1cf49390f8b7229a33e82b9fd381a7
TheGreatGospel <david.kaen.chan@gmail.com>
2016-12-14 17:43:15 +1300

    asdf

--- scripts/options.js
+++ scripts/options.js
@@ -1,4 +1,35 @@
+function options_Validate() {
+        var errorMsg = "";
+        var inpObj = {
+            popSize: parseInt($("#options_popSize").val(), 10),
+            numOfPop: parseInt($("#options_numOfPop").val(), 10),
+            init: parseInt($("#options_init").val(), 10)
+        };
 
+        errorMsg += conditionHack(isNaN(inpObj.popSize),
+            "The \"Population Size\" has an invalid input! \n",
+            inpObj.popSize < 50 || inpObj.popSize > 500,
+            "The \"Population Size\" has an invalid input! \n");
+        
+        errorMsg += conditionHack(isNaN(inpObj.numOfPop),
+            "The \"Number of Populations\" has an invalid input! \n",
+            inpObj.numOfPop < 1 || inpObj.numOfPop > 10,
+            "The \"Number of Populations\" has an invalid input! \n");
+        
+        errorMsg += conditionHack(isNaN(inpObj.init),
+            "The \"Initial Number of Simulations\" has an invalid input! \n",
+            inpObj.init < 1,
+            "The \"Initial Number of Simulations\" has an invalid input! \n");
+
+        if (errorMsg === "") {
+            $("#demo2").html("hi");
+            settings = inpObj;
+            $("#demo2").html("hi2");
+            loadSimulation();
+        } else {
+            alert(errorMsg);
+        }
+    };
 
 /*
 document.getElementById("demo").innerHTML =  typeof inpObj.popSize + " " +
